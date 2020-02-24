cc42e7051bec053dd63dd25d3fb90e463e3a8016
BiPi2016 <bp.singh@hotmail.com>
2020-02-24 13:44:47 +0100

    filters handwritten

--- src/components/filters/filters.js
+++ src/components/filters/filters.js
@@ -24,11 +24,65 @@ library.add(
 
 
   const filters =  [{
-    name: 'Price',
-    title: 'Pris',
+    name: 'priceFrom',
+    title: 'Pris Från',
     icon: faDollarSign,
     optionList: [1000, 2000, 3000, 5000],
   },
+  {
+    name: 'priceTo',
+    title: 'Pris Till',
+    icon: faDollarSign,
+    optionList: [1000, 2000, 3000, 5000]
+  },
+  {
+    name: 'yearTo',
+    title: 'årsmodell från',
+    icon: faCalendarCheck,
+    optionList: [2020, 2019, 2018, 2017]
+  },
+  {
+    name: 'yearTo',
+    title: 'årsmodell till',
+    icon: faCalendarCheck,
+    optionList: [2020, 2019, 2018, 2017]
+  },
+  {
+    name: 'gearbox',
+    title: 'växellåda',
+    icon: faGamepad,
+    optionList: [manuell, automatisk]
+  },
+  {
+    name: 'fuelType',
+    title: 'drivmedel',
+    icon: faBatteryThreeQuarters,
+    optionList: [diesel, bensin, elektrik, hybrid]
+  },
+  {
+    name: 'meterFrom',
+    title: 'mätarställning från',
+    icon: faTachometerAlt,
+    optionList: [1000, 2000, 3000, 5000]
+  },
+  {
+    name: 'meterTill',
+    title: 'mätarställning till',
+    icon: faTachometerAlt,
+    optionList: [1000, 2000, 3000, 5000]
+  },
+  {
+    name: 'horsePowerFrom',
+    title: 'hästkraft från',
+    icon: faTruckMonster,
+    optionList: [40, 60, 80, 100]
+  },
+  {
+    name: 'horsePowerTo',
+    title: 'hästkraft till',
+    icon: faTruckMonster,
+    optionList: [40, 60, 80, 100]
+  }
 ];
 
 const Filters = () => {
